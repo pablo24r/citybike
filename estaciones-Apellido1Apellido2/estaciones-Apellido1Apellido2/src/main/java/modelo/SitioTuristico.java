87ee3c429e0e96980336a2e07b398413a5a2aaf3
pablo24r <94908598+pablo24r@users.noreply.github.com>
2023-10-20 17:52:16 +0200

    prueba

---
+++ estaciones-Apellido1Apellido2/estaciones-Apellido1Apellido2/src/main/java/modelo/SitioTuristico.java
@@ -0,0 +1,20 @@
+package modelo;
+
+public class SitioTuristico {
+
+	private String nombre;
+	private String descripcion;
+	private double distancia;
+	private String URL;
+	
+	
+	public SitioTuristico(String nombre, String descripcion, double distancia, String uRL) {
+		super();
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.distancia = distancia;
+		URL = uRL;
+	}
+	
+	
+}
