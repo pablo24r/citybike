287833e3bfdd31a84f48e32795b5c23b1b59952b
JoaquinCarrillo17 <joaquin.carrilloy@um.es>
2023-10-25 16:17:30 +0200

    Json-b a falta de probarlo

--- estaciones-Apellido1Apellido2/src/main/java/repositorios/RepositorioJSON.java
+++ estaciones-Apellido1Apellido2/src/main/java/repositorios/RepositorioJSON.java
@@ -1,43 +1,146 @@
 package repositorios;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
 
-public class RepositorioJSON<T extends Identificable> implements RepositorioString<T> {
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+import javax.json.bind.JsonbConfig;
+import javax.json.bind.config.PropertyNamingStrategy;
+import javax.json.bind.config.PropertyOrderStrategy;
 
-	@Override
-	public String add(T entity) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
-	public void update(T entity) {
-		// TODO Auto-generated method stub
-		
-	}
+public abstract class RepositorioJSON<T extends Identificable> implements RepositorioString<T> {
 
-	@Override
-	public void delete(T entity) {
-		// TODO Auto-generated method stub
-		
-	}
+    public final static String DIRECTORIO = "repositorio-json/";
 
-	@Override
-	public T getById(String id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    static {
 
-	@Override
-	public List<T> getAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-//
-	@Override
-	public List<String> getIds() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        File directorio = new File(DIRECTORIO);
 
+        if (!directorio.exists()) {
+            directorio.mkdir();
+        }
+    }
+
+    // Método abstracto que se delega a los repositorios específicos
+    public abstract Class<T> getClase();
+
+    /*** Métodos de apoyo ***/
+
+    protected String getDocumento(String id) {
+        return DIRECTORIO + getClase().getSimpleName() + "-" + id + ".json";
+    }
+
+    protected boolean checkDocumento(String id) {
+        final String documento = getDocumento(id);
+        File fichero = new File(documento);
+        return fichero.exists();
+    }
+
+    protected void save(T entity) throws RepositorioException {
+        final String documento = getDocumento(entity.getId());
+
+        try {
+            JsonbConfig config = new JsonbConfig()
+                    .withNullValues(true)
+                    .withFormatting(true)
+                    .withPropertyNamingStrategy(PropertyNamingStrategy.LOWER_CASE_WITH_UNDERSCORES)
+                    .withPropertyOrderStrategy(PropertyOrderStrategy.LEXICOGRAPHICAL);
+
+            Jsonb jsonb = JsonbBuilder.create(config);
+            String jsonString = jsonb.toJson(entity);
+            try (FileOutputStream fos = new FileOutputStream(documento)) {
+                fos.write(jsonString.getBytes());
+            }
+        } catch (Exception e) {
+            throw new RepositorioException("Error al guardar la entidad con id: " + entity.getId(), e);
+        }
+    }
+
+    protected T load(String id) throws RepositorioException, EntidadNoEncontrada {
+        if (!checkDocumento(id)) {
+            throw new EntidadNoEncontrada("La entidad no existe, id: " + id);
+        }
+        final String documento = getDocumento(id);
+
+        try {
+            JsonbConfig config = new JsonbConfig()
+                    .withNullValues(true)
+                    .withFormatting(true)
+                    .withPropertyNamingStrategy(PropertyNamingStrategy.LOWER_CASE_WITH_UNDERSCORES)
+                    .withPropertyOrderStrategy(PropertyOrderStrategy.LEXICOGRAPHICAL);
+
+            Jsonb jsonb = JsonbBuilder.create(config);
+            Reader reader = new FileReader(documento);
+            return jsonb.fromJson(reader, getClase());
+        } catch (Exception e) {
+            throw new RepositorioException("Error al cargar la entidad con id: " + id, e);
+        }
+    }
+
+    /*** Fin métodos de apoyo ***/
+
+    @Override
+    public String add(T entidad) throws RepositorioException {
+        String id = UUID.randomUUID().toString();
+        entidad.setId(id);
+        save(entidad);
+        return id;
+    }
+
+    @Override
+    public void update(T entidad) throws RepositorioException, EntidadNoEncontrada {
+        if (!checkDocumento(entidad.getId())) {
+            throw new EntidadNoEncontrada("La entidad no existe, id: " + entidad.getId());
+        }
+        save(entidad);
+    }
+
+    @Override
+    public void delete(T entidad) throws EntidadNoEncontrada {
+        if (!checkDocumento(entidad.getId())) {
+            throw new EntidadNoEncontrada("La entidad no existe, id: " + entidad.getId());
+        }
+        final String documento = getDocumento(entidad.getId());
+        File fichero = new File(documento);
+        fichero.delete();
+    }
+
+    @Override
+    public T getById(String id) throws RepositorioException, EntidadNoEncontrada {
+        return load(id);
+    }
+
+    @Override
+    public List<String> getIds() {
+        LinkedList<String> resultado = new LinkedList<>();
+        File directorio = new File(DIRECTORIO);
+        File[] entidades = directorio.listFiles(f -> f.isFile() && f.getName().endsWith(".json"));
+        final String prefijo = getClase().getSimpleName() + "-";
+        for (File file : entidades) {
+            String id = file.getName().substring(prefijo.length(), file.getName().length() - 5); 
+            resultado.add(id);
+        }
+        return resultado;
+    }
+
+    @Override
+    public List<T> getAll() throws RepositorioException {
+        LinkedList<T> resultado = new LinkedList<T>();
+        for (String id : getIds()) {
+            try {
+                resultado.add(load(id));
+            } catch (EntidadNoEncontrada e) {
+                // no debería suceder
+                e.printStackTrace();
+            }
+        }
+        return resultado;
+    }
 }
